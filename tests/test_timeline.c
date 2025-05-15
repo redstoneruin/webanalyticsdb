f455a743180a6270976a3f45a65d1f47286465e5
Ryan Steinwert <ryansteinwert@gmail.com>
2025-05-14 20:32:03 -0700

    better testing

--- tests/test_timeline.c
+++ tests/test_timeline.c
@@ -1,12 +1,23 @@
 #include "../src/timeline.h"
 #include "../test/unity/unity.h"
 
+#include <stdio.h>
+#include <unistd.h>
+
+#define TEST_ID 0xFFFFFFFF
+
 void setUp(void) {}
 void tearDown(void) {}
 
+void delete_data_file() {
+    char data_path[128];
+    sprintf(data_path, "data/timeline_%u.dat", TEST_ID);
+    remove(data_path);
+}
+
 void test_timeline_init() {
     Timeline t;
-    TEST_ASSERT_FALSE(init_timeline(&t, 0));
+    TEST_ASSERT_FALSE(init_timeline(&t, TEST_ID));
     TEST_ASSERT(t.length == 0);
     TEST_ASSERT(t.first_unwritten == 0);
     TEST_ASSERT(t.events);
@@ -15,7 +26,7 @@ void test_timeline_init() {
 
 void test_timeline_append_1() {
     Timeline t;
-    TEST_ASSERT_FALSE(init_timeline(&t, 0));
+    TEST_ASSERT_FALSE(init_timeline(&t, TEST_ID));
 
     TEST_ASSERT_FALSE(
         append_event_to_timeline(&t, 0, 0)
@@ -29,7 +40,7 @@ void test_timeline_append_1() {
 
 void test_timeline_append_2() {
     Timeline t;
-    TEST_ASSERT_FALSE(init_timeline(&t, 0));
+    TEST_ASSERT_FALSE(init_timeline(&t, TEST_ID));
 
     for (int i = 0; i != 5001; ++i) {
         TEST_ASSERT_FALSE(
@@ -48,7 +59,7 @@ void test_timeline_append_2() {
 
 void test_timeline_write_to_disk_1() {
     Timeline t;
-    TEST_ASSERT_FALSE(init_timeline(&t, 0));
+    TEST_ASSERT_FALSE(init_timeline(&t, TEST_ID));
 
     for (int i = 0; i != 5001; ++i) {
         append_event_to_timeline(&t, 0, 0);
@@ -58,17 +69,24 @@ void test_timeline_write_to_disk_1() {
     TEST_ASSERT(t.first_unwritten == 5001);
 
     destroy_timeline(&t);
+    delete_data_file();
 }
 
 void test_timeline_read_from_disk_1() {
     Timeline t;
-    TEST_ASSERT_FALSE(init_timeline(&t, 0));
+    TEST_ASSERT_FALSE(init_timeline(&t, TEST_ID));
+
+    for (int i = 0; i != 5001; ++i) {
+        append_event_to_timeline(&t, 0, 0);
+    }
+    write_timeline_to_disk(&t);
 
     TEST_ASSERT_FALSE(read_timeline_from_disk(&t));
-    TEST_ASSERT(t.events[0].path == 0 && t.events[0].type == 0);
-    TEST_ASSERT(t.length > 0 && t.allocated > 0);
+    TEST_ASSERT(t.length == 5001 && t.allocated >= 5001);
+    TEST_ASSERT(t.events[5000].path == 0 && t.events[5000].type == 0);
 
     destroy_timeline(&t);
+    delete_data_file();
 }
 
 int main() {
